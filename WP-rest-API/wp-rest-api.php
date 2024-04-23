ce016c900cde6ec1aa6fcae8557b0df0a6f383c4
Robeeen <robeeen@gmail.com>
2024-04-23 21:25:04 +0600

    update

---
+++ WP-rest-API/wp-rest-api.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: WP REST API
+Plugin URI: https://shamskhan.xyz
+Description: create db tables, then insert some data, fetch the data from postman and insert some data from post using rest api.
+Author: Shams Khan
+Version: 1.0.0
+Author URI: https://shamskhan.xyz
+*/
+
+// Exit if accessed directly
+if( !defined('ABSPATH'))
+{
+    exit; 
+}
+
+register_activation_hook( __FILE__, 'setup_table');
+
+//setup table for api data
+function setup_table(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'form_submission';
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        name varchar (100) NOT NULL,
+        email varchar (100) NOT NULL,
+        PRIMARY KEY (id)
+        )";
+    
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta( $sql );
+}
+
